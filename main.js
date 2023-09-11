a80a49a3a223a345bebf834a507fe6ffe4904a82
heejung1180 <heejung1180@outlook.com>
2023-09-10 22:18:12 -0700

    html and css setup

---
+++ main.js
@@ -0,0 +1,27 @@
+/*-------- constants --------*/
+
+
+/*----- state variables -----*/
+// turn: use 1 and -1
+// board: 2D array
+  // null to represent no player in particular cell
+  // cell to hold 1 or -1
+// winner: 
+  // null to represent no winner
+  // 1 or -1 winner
+  // 'T' for tie
+
+
+/*----- cached elements -----*/
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+/*-------- functions --------*/
