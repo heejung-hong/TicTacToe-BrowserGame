cb80f578bd78fb8efab9f05789480672c3fdd7a3
heejung1180 <heejung1180@outlook.com>
2023-09-10 22:54:27 -0700

    set up state variables and init function

--- main.js
+++ main.js
@@ -2,11 +2,14 @@
 
 
 /*----- state variables -----*/
-// turn: use 1 and -1
 // board: 2D array
+let board; 
   // null to represent no player in particular cell
   // cell to hold 1 or -1
-// winner: 
+// turn: use 1 and -1
+let turn;
+  // winner: 
+let winner;
   // null to represent no winner
   // 1 or -1 winner
   // 'T' for tie
@@ -25,3 +28,24 @@
 
 
 /*-------- functions --------*/
+// call the init function
+init()
+// initialize all state, then call render() to visualize the state in the DOM
+function init() {
+  // to visualize the board's mapping to the DOM,
+  // rotate the board array 90 degrees counter-clockwise
+  board = [ // no let or const to use the state variable above
+    [0, 0, 0], // col 0
+    [0, 0, 0], // col 1
+    [0, 0, 0]  // col 2
+  ];
+  // when game starts, it will be player one 
+  turn = 1;
+  // when game starts, there is no winner
+  winner = null;
+  render();
+}
+
+function render() {
+  
+}
